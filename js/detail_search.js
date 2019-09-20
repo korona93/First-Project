769911a963a0b275bb9beef3489e435f21b44d2f
hajoonyang <54052572+hajoonyang@users.noreply.github.com>
2019-09-20 13:21:25 +0900

    commit

--- js/detail_search.js
+++ js/detail_search.js
@@ -1,4 +1,6 @@
+// 네비바 열고 닫기
 $(document).ready(function(){
+
     //var state = true;
 	$("#navBtn").click(function(){
         $(".navContentWrap").stop().slideToggle("fast");
@@ -28,8 +30,17 @@ $(document).ready(function(){
 	// 	img.attr("src",img.attr("src").replace("off", "on")).attr("alt","전체메뉴열기");
 	// }	
 
-
-
 });	
 
-			
+// 네비바 금액에 ',' 삽입!
+	// addComma() : 숫자 타입값을 매개변수로 받아 콤마를 추가하여 문자열로 반환해주는 함수
+function addComma(num) {
+	var regexp = /\B(?=(\d{3})+(?!\d))/g;
+	return num.toString().replace(regexp, ',');
+}
+$(function(){
+	// console.log(typeof $('.selectproduct_price').text());
+	$('.selectproduct_price').each(function () {
+		$(this).text(addComma($(this).text()));
+	});
+});
