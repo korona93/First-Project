d8b1d713cf65cb48c2d78a19fe0df597d5196b86
hajoonyang <54052572+hajoonyang@users.noreply.github.com>
2019-09-24 13:19:23 +0900

    commit

--- js/detail_search.js
+++ js/detail_search.js
@@ -1,51 +1,11 @@
 // 네비바 열고 닫기
 $(document).ready(function(){
 
-    //var state = true;
 	$("#navBtn").click(function(){
         $(".navContentWrap").stop().slideToggle("fast");
-        /*
-        $(".navContentWrap")..slidetoggle("fast");
-		if(!state){			
-			// imgOff(this);
-			state = true;
-		}else{			
-			// imgOn(this);
-			state = false;
-		}				
-        return false;
-        */
+		// 이미지 변환 넣어야함!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	});
-	// $("#close_bt").on("click blur",function(){				
-	// 	$("#all_menu").stop().slideUp("fast");
-	// 	imgOn("#open_bt");
-	// 	state = false;				
-	// });	
-	// function imgOff(t){				
-	// 	var img = $(t).find("img");				
-	// 	img.attr("src",img.attr("src").replace("on", "off")).attr("alt","전체메뉴닫기");
-	// }
-	// function imgOn(t){				
-	// 	var img = $(t).find("img");
-	// 	img.attr("src",img.attr("src").replace("off", "on")).attr("alt","전체메뉴열기");
-	// }	
-
 });	
-
-// 네비바 금액에 ',' 삽입!
-	// addComma() : 숫자 타입값을 매개변수로 받아 콤마를 추가하여 문자열로 반환해주는 함수
-function addComma(num) {
-	var regexp = /\B(?=(\d{3})+(?!\d))/g;
-	return num.toString().replace(regexp, ',');
-}
-$(function(){
-	// console.log(typeof $('.selectproduct_price').text());
-	$('.selectproduct_price').each(function () {
-		$(this).text(addComma($(this).text()));
-	});
-});
-
-
 
 $(function(){
 
@@ -65,11 +25,34 @@ $(function(){
 		li.remove();
 	});
 
-	// // totalprice 변경
-	 $(".selectproduct_price").show(function(){
-	 	var each = $(this).$('inputNumber').val() * $(this).$('selectproduct_price').val();
-	 	var total = 
-	});
-	$(인풋).each(change)
+	// totalprice 변경
+	var totalP=0;
+	for(var i=1; i<=3; i++){
+		var price = Number($("#prod"+i).text());
+		var eachT = price*$("#inp"+i).val();
+		totalP += eachT;
+	}
+	$("#total").text(totalP);	  
 
+});
+// 인풋변경될때마다 토탈프라이스 함수 실행되는 이벤트 작성하기!!!!!!!!!!!!!!!!!!!!!!!!!!
+	// var allinput = new Array;
+	// for(var i=1; i<=3; i++){
+	// 	allinput = {$("#inp"+i).val();}
+	// }
+	
+
+
+
+// 네비바 금액에 ',' 삽입!
+	// addComma() : 숫자 타입값을 매개변수로 받아 콤마를 추가하여 문자열로 반환해주는 함수
+function addComma(num) {
+	var regexp = /\B(?=(\d{3})+(?!\d))/g;
+	return num.toString().replace(regexp, ',');
+}
+$(function(){
+		// console.log(typeof $('.selectproduct_price').text());
+	$('.selectproduct_price').each(function () {
+		$(this).text(addComma($(this).text()));
+	});
 });
