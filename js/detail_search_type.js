559e70e93f5dc3b771f07a8299ddd73750505a7f
shs2674 <54052367+shs2674@users.noreply.github.com>
2019-10-21 14:17:13 +0900

    commit

--- js/detail_search_type.js
+++ js/detail_search_type.js
@@ -4,96 +4,104 @@ $(document).ready(function() {
         if(chkValue == '1'){
             $('#type .fashion').css('display','block');
             $('#type .cosmetic').css('display','none');
-            $('input[name="type"]').removeAttr('checked');
+            $('input[name="type2"]').removeAttr('checked');
+            $('#subType2 div').css('display','none');
         }else if(chkValue=='2'){
             $('#type .fashion').css('display','none');
             $('#type .cosmetic').css('display','block');
-            $('input[name="type"]').removeAttr('checked');
-            $('#subType div').css('display','none');
+            $('input[name="type1"]').removeAttr('checked');
+            $('#subType1 div').css('display','none');
         }
     });
-    $('input[name=type]').on('click',function(){
-        var chkValue = $('input[name=type]:checked').val();
-        $('#subType .menu3').css('display','block');
+    $('input[name=type1]').on('click',function(){
+        var chkValue = $('input[name=type1]:checked').val();
+        $('#subType1 .menu3').css('display','block');
         if(chkValue == '1'){
-            $('#subType .fs1').css('display','block');
-            $('#subType .fs2').css('display','none');
-            $('#subType .fs3').css('display','none');
-            $('#subType .fs4').css('display','none');
-            $('#subType .fs5').css('display','none');
-            $('#subType .fs6').css('display','none');
-            $('#subType .fs7').css('display','none');
-            $('#subType .fs8').css('display','none');
+            $('#subType1 .fs1').css('display','block');
+            $('#subType1 .fs2').css('display','none');
+            $('#subType1 .fs3').css('display','none');
+            $('#subType1 .fs4').css('display','none');
+            $('#subType1 .fs5').css('display','none');
+            $('#subType1 .fs6').css('display','none');
+            $('#subType1 .fs7').css('display','none');
+            $('#subType1 .fs8').css('display','none');
         }
         else if(chkValue == '2'){
-            $('#subType .fs1').css('display','none');
-            $('#subType .fs2').css('display','block');
-            $('#subType .fs3').css('display','none');
-            $('#subType .fs4').css('display','none');
-            $('#subType .fs5').css('display','none');
-            $('#subType .fs6').css('display','none');
-            $('#subType .fs7').css('display','none');
-            $('#subType .fs8').css('display','none');
+            $('#subType1 .fs1').css('display','none');
+            $('#subType1 .fs2').css('display','block');
+            $('#subType1 .fs3').css('display','none');
+            $('#subType1 .fs4').css('display','none');
+            $('#subType1 .fs5').css('display','none');
+            $('#subType1 .fs6').css('display','none');
+            $('#subType1 .fs7').css('display','none');
+            $('#subType1 .fs8').css('display','none');
         }
         else if(chkValue == '3'){
-            $('#subType .fs1').css('display','none');
-            $('#subType .fs2').css('display','none');
-            $('#subType .fs3').css('display','block');
-            $('#subType .fs4').css('display','none');
-            $('#subType .fs5').css('display','none');
-            $('#subType .fs6').css('display','none');
-            $('#subType .fs7').css('display','none');
-            $('#subType .fs8').css('display','none');
+            $('#subType1 .fs1').css('display','none');
+            $('#subType1 .fs2').css('display','none');
+            $('#subType1 .fs3').css('display','block');
+            $('#subType1 .fs4').css('display','none');
+            $('#subType1 .fs5').css('display','none');
+            $('#subType1 .fs6').css('display','none');
+            $('#subType1 .fs7').css('display','none');
+            $('#subType1 .fs8').css('display','none');
         }
         else if(chkValue == '4'){
-            $('#subType .fs1').css('display','none');
-            $('#subType .fs2').css('display','none');
-            $('#subType .fs3').css('display','none');
-            $('#subType .fs4').css('display','block');
-            $('#subType .fs5').css('display','none');
-            $('#subType .fs6').css('display','none');
-            $('#subType .fs7').css('display','none');
-            $('#subType .fs8').css('display','none');
+            $('#subType1 .fs1').css('display','none');
+            $('#subType1 .fs2').css('display','none');
+            $('#subType1 .fs3').css('display','none');
+            $('#subType1 .fs4').css('display','block');
+            $('#subType1 .fs5').css('display','none');
+            $('#subType1 .fs6').css('display','none');
+            $('#subType1 .fs7').css('display','none');
+            $('#subType1 .fs8').css('display','none');
         }
         else if(chkValue == '5'){
-            $('#subType .fs1').css('display','none');
-            $('#subType .fs2').css('display','none');
-            $('#subType .fs3').css('display','none');
-            $('#subType .fs4').css('display','none');
-            $('#subType .fs5').css('display','block');
-            $('#subType .fs6').css('display','none');
-            $('#subType .fs7').css('display','none');
-            $('#subType .fs8').css('display','none');
+            $('#subType1 .fs1').css('display','none');
+            $('#subType1 .fs2').css('display','none');
+            $('#subType1 .fs3').css('display','none');
+            $('#subType1 .fs4').css('display','none');
+            $('#subType1 .fs5').css('display','block');
+            $('#subType1 .fs6').css('display','none');
+            $('#subType1 .fs7').css('display','none');
+            $('#subType1 .fs8').css('display','none');
         }
         else if(chkValue == '6'){
-            $('#subType .fs1').css('display','none');
-            $('#subType .fs2').css('display','none');
-            $('#subType .fs3').css('display','none');
-            $('#subType .fs4').css('display','none');
-            $('#subType .fs5').css('display','none');
-            $('#subType .fs6').css('display','block');
-            $('#subType .fs7').css('display','none');
-            $('#subType .fs8').css('display','none');
+            $('#subType1 .fs1').css('display','none');
+            $('#subType1 .fs2').css('display','none');
+            $('#subType1 .fs3').css('display','none');
+            $('#subType1 .fs4').css('display','none');
+            $('#subType1 .fs5').css('display','none');
+            $('#subType1 .fs6').css('display','block');
+            $('#subType1 .fs7').css('display','none');
+            $('#subType1 .fs8').css('display','none');
         }
         else if(chkValue == '7'){
-            $('#subType .fs1').css('display','none');
-            $('#subType .fs2').css('display','none');
-            $('#subType .fs3').css('display','none');
-            $('#subType .fs4').css('display','none');
-            $('#subType .fs5').css('display','none');
-            $('#subType .fs6').css('display','none');
-            $('#subType .fs7').css('display','block');
-            $('#subType .fs8').css('display','none');
+            $('#subType1 .fs1').css('display','none');
+            $('#subType1 .fs2').css('display','none');
+            $('#subType1 .fs3').css('display','none');
+            $('#subType1 .fs4').css('display','none');
+            $('#subType1 .fs5').css('display','none');
+            $('#subType1 .fs6').css('display','none');
+            $('#subType1 .fs7').css('display','block');
+            $('#subType1 .fs8').css('display','none');
         }
         else if(chkValue == '8'){
-            $('#subType .fs1').css('display','none');
-            $('#subType .fs2').css('display','none');
-            $('#subType .fs3').css('display','none');
-            $('#subType .fs4').css('display','none');
-            $('#subType .fs5').css('display','none');
-            $('#subType .fs6').css('display','none');
-            $('#subType .fs7').css('display','none');
-            $('#subType .fs8').css('display','block');
+            $('#subType1 .fs1').css('display','none');
+            $('#subType1 .fs2').css('display','none');
+            $('#subType1 .fs3').css('display','none');
+            $('#subType1 .fs4').css('display','none');
+            $('#subType1 .fs5').css('display','none');
+            $('#subType1 .fs6').css('display','none');
+            $('#subType1 .fs7').css('display','none');
+            $('#subType1 .fs8').css('display','block');
+        }
+    });
+    $('input[name=type2]').on('click',function(){
+        var chkValue = $('input[name=type2]:checked').val();
+        $('#subType .menu3').css('display','block');
+        if(chkValue == '1'){
+            $('#subtype .cm1').css('display','block');
         }
     });
 });
