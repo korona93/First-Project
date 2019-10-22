05a963000489453e908364d50ae65592b27561f1
shs2674 <54052367+shs2674@users.noreply.github.com>
2019-10-22 22:18:54 +0900

    commit

--- js/detail_search_type.js
+++ js/detail_search_type.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
             $('#type .fashion').css('display','block');
             $('#type .cosmetic').css('display','none');
             $('input[name="type2"]').removeAttr('checked');
+            $('input[name="subType2"]').removeAttr('checked');
             $('#subType1').css('display','block');
             $('#subType2').css('display','none');
             $('#subType2 div').css('display','none');
@@ -14,15 +15,17 @@ $(document).ready(function() {
             $('#type .fashion').css('display','none');
             $('#type .cosmetic').css('display','block');
             $('input[name="type1"]').removeAttr('checked');
+            $('input[name="subType1"]').removeAttr('checked');
             $('#subType1').css('display','none');
             $('#subType2').css('display','block');
             $('#subType1 div').css('display','none');
         }
     });
-    /* sype에 대한 js */
+    /* subType에 대한 js */
     $('input[name=type1]').on('click',function(){
         var chkValue = $('input[name=type1]:checked').val();
         $('#subType1 .menu3').css('display','block');
+        $('input[name="subType1"]').removeAttr('checked');
         if(chkValue == '1'){
             $('#subType1 .fs1').css('display','block');
             $('#subType1 .fs2').css('display','none');
@@ -107,6 +110,7 @@ $(document).ready(function() {
     $('input[name=type2]').on('click',function(){
         var chkValue = $('input[name=type2]:checked').val();
         $('#subType2 .menu3').css('display','block');
+        $('input[name="subType2"]').removeAttr('checked');
         if(chkValue == '1'){
             $('#subType2 .cm1').css('display','block');
             $('#subType2 .cm2').css('display','none');
@@ -209,3 +213,22 @@ $(document).ready(function() {
     });
 });
 
+
+
+$(document).ready(function(){
+
+    var select_array= Array();
+    var select_cnt = 0;
+    var chkbox = $('input[name=subType1]');
+
+    for(i=0; i<chkbox.length; i++){
+        if(chkbox[i].checked==true){
+            select_array[select_cnt]=chkbox[i].value;
+            select_cnt++;
+        }
+    }
+    /* https://neptune297.tistory.com/60  사용한거*/
+    console.log(select_array);
+    
+
+});
